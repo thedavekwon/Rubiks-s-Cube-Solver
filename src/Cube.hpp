ca5fd72e9404b1261460fb87cf7696ea658cdd57
dhyunpark <danpark0605@gmail.com>
2019-01-07 23:20:10 +0900

    finished left rotation

--- src/Cube.hpp
+++ src/Cube.hpp
@@ -16,7 +16,7 @@ private:
     vector<vector<int>> Right;
     vector<vector<int>> Front;
     vector<vector<int>> Back;
-    
+
 public:
     Cube() {
         Up = vector<vector<int>>(3, vector<int>(3, 0));
@@ -48,7 +48,7 @@ public:
         Front[3] = Left[3];
         Left[3] = Back[3];
         Back[3] = Right[3];
-        Front[3] = tmp;                                                                   
+        Front[3] = tmp;
     }
     void turnCounterDown() {
         vector<int> tmp = Front[3];
@@ -59,19 +59,27 @@ public:
     }
 
     void turnLeft() {
+        //non face turns
         vector<int> tmp = {Front[0][0], Front[1][0], Front[2][0]};
         Front[0][0] = Up[0][0]; Front[1][0] = Up[1][0]; Front[2][0] = Up[2][0];
-                        
+        Up[0][0] = Back[2][2]; Up[1][0] = Back[1][2]; Up[2][0] = Back[0][2];
+        Back[0][2] = Down[2][0]; Back[1][2] = Down[1][0]; Back[2][2] = Down[0][0];
+        Down[0][0] =  tmp[0]; Down[1][0] = tmp[1]; Down[2][0] = tmp[2];
     }
     void turnCounterLeft() {
-
-    } 
+        //non face turns
+        vector<int> tmp = {Front[0][0], Front[1][0], Front[2][0]};
+        Front[0][0] = Down[0][0]; Front[1][0] = Down[2][0]; Front[2][0] = Down[2][0];
+        Down[0][0] = Back[2][2]; Down[1][0] = Back[1][2]; Down[2][0] = Back[0][2];
+        Back[0][2] = Up[2][0]; Back[1][2] = Up[1][0]; Back[2][2] = Up[0][0];
+        Up[0][0] = tmp[0]; Up[1][0] = tmp[1]; Up[2][0] = tmp[2];
+    }
 
     void turnRight() {
 
     }
     void turnCounterRight(){
-        
+
     }
 
 
@@ -80,7 +88,7 @@ public:
         Up[2] = {Left[2][2], Left[1][2], Left[0][2]};
         Left[0][2] = Down[0][0]; Left[1][2] = Down[0][1]; Left[2][2] = Down[0][2];
         Down[0] = {Right[2][0], Right[1][0], Right[0][0]};
-        Right[0][1] = tmp[0]; Right[1][0] = tmp[1]; Right[2][0] = tmp[2];      
+        Right[0][1] = tmp[0]; Right[1][0] = tmp[1]; Right[2][0] = tmp[2];
     }
 
     void turnCounterFront() {
@@ -97,7 +105,7 @@ public:
     }
 
     void showTable(){
-        
+
     }
 };
 
